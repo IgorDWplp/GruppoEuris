486c7fa7172a8dcb5183da850e67cfc51c624682
Igor <igzyy.j@gmail.com>
2020-04-18 11:36:51 +0200

    Working in progress 1
    Nakon niz problema s VS i SQL te zadnjim update win konačno sam uspio stat na noge s projektom.

---
+++ EURISTest_vs2015_EN/EURISTest/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EURIS.Service;
+using EURIS.Entities;
+using EURIS.Entities.Model;
+
+namespace EURIS.Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            DbContextApp dbContextApp = new DbContextApp();
+            ViewBag.Message = "Welcome to the EURIS Group ASP.NET MVC developer test application.";
+            return View();
+        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your app description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+
+    }
+}
---
+++ EURISTest_vs2015_EN/EURISTest/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EURIS.Service;
+using EURIS.Entities;
+
+namespace EURISTest.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductManager productManager = new ProductManager();
+        LocalDbEntities LocalDbEntities = new LocalDbEntities();
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            List<Product> products = productManager.GetProducts();
+            ViewBag.Products = products;
+            return View();
+
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product pro)
+        {
+            if (ModelState.IsValid)
+            {
+                productManager.CreateNew(pro);
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var model = productManager.GetProduct(Id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int Id)
+        {
+            var model = productManager.GetProduct(Id);
+            return View(model);
+        }
+
+    }
+}
